37e598aae92819548e2045426280d9b3d075497f
abdOfHossein <behzadshafieejoon1999@yahoo.com>
2022-06-16 06:52:53 -0700

    p5

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,16 @@ import { AppModule } from "./app.module";
 import { NestExpressApplication } from "@nestjs/platform-express";
 import { join } from "path";
 import { ValidationPipe } from "@nestjs/common";
-
+const hbs = require("hbs");
+const path = require("path");
 
 
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule,{bodyParser:false});
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, { bodyParser: false });
   app.useStaticAssets(join(__dirname, '..', "public"))
   app.setBaseViewsDir(join(__dirname, '..', 'views'))
+  hbs.registerPartials(join(__dirname + '..'+'/views/partials'));
   app.setViewEngine('hbs');
   app.useGlobalPipes(new ValidationPipe())
   await app.listen(3000)
